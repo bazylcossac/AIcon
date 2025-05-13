bed73d2cf715f5fe0ed75f2f54f5b5b3042eee6f
bazylcossac <bazyl.cossac@gmail.com>
2025-05-13 17:55:58 +0200

    finish text to speech page

--- src/app/(main)/dashboard/(products)/text-to-speech/page.tsx
+++ src/app/(main)/dashboard/(products)/text-to-speech/page.tsx
@@ -14,7 +14,7 @@ import { Slider } from "@/components/ui/slider";
 function Page() {
   return (
     <section className="h-full w-full flex">
-      <div className="flex-1">
+      <div className="flex-1 flex-col justify-between">
         <div className="flex items-center justify-between py-4 border-b-1 border-black px-4">
           <p className="text-lg font-bold">Text to speech</p>
           <div className="flex flex-row items-center gap-1 text-white/70 hover:text-white transition">
@@ -22,6 +22,21 @@ function Page() {
             <Link href="/dashboard/storage" className="text-xs ">
               Storage
             </Link>
+          </div>
+        </div>
+
+        <div className="h-9/12 flex items-center justify-center">
+          <p className="font-bold ">Generated speech will apear here</p>
+        </div>
+        <div className="mx-14 rounded-2xl flex flex-col  mb-10 border-1 border-white/50 focus:border-green-300 focus:border-2 transition">
+          <textarea
+            className="w-full  p-4 rounded-2xl resize-none outline-none custom-scrollbar"
+            placeholder="Enter your message..."
+          ></textarea>
+          <div className="flex justify-end m-2">
+            <button className="bg-green-700 p-2 rounded-md hover:bg-green-800 transition cursor-pointer">
+              Generate
+            </button>
           </div>
         </div>
       </div>
