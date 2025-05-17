65efc7ea1d433f363e6b3468d369f813017ee7be
bazylcossac <bazyl.cossac@gmail.com>
2025-05-17 10:10:29 +0200

    add test types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -14,3 +14,30 @@ export type ActionType =
   | { type: "SET_VOICE"; payload: string }
   | { type: "SET_FORMAT"; payload: "mp3" | "wav" }
   | { type: "SET_MESSAGE"; payload: string };
+
+type complex = {
+  a: string;
+  b: string;
+} & {
+  c: boolean;
+} & {
+  d: string[];
+};
+
+type Prettify<T> = {
+  [K in keyof T]: T[K];
+};
+type test = Prettify<complex>;
+
+type foo = "a" | "b" | 1;
+
+type bar = Exclude<foo, string>;
+
+type Foo = {
+  name: string;
+  age: string;
+};
+
+type getFoo = {
+  [K in keyof Foo as `get${Capitalize<K>}`]: () => Foo[K];
+};
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,6 +1,13 @@
 import { create } from "zustand";
 import { GeneratedImageType } from "./storeTypes";
 
-export const useStore = create((set, get) => ({
+type mainStore = {
+  generatedImages: GeneratedImageType[];
+  addToGeneratedImages: (genImage: GeneratedImageType) => void;
+};
+
+export const useStore = create<mainStore>((set, get) => ({
   generatedImages: [] as GeneratedImageType[],
+  addToGeneratedImages: (genImage: GeneratedImageType) =>
+    set((state) => ({ generatedImages: [...state.generatedImages, genImage] })),
 }));
